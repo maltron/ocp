02bb3c8307f12ebbc91bf39cbe123c48985c8ef4
Mauricio Leal <mauricio@whitemaltron.local>
2020-08-04 15:40:13 -0300

    Modified ocp-console.sh to adapt to new logging from OCP 4.5

--- ocp-console.sh
+++ ocp-console.sh
@@ -1,2 +1,2 @@
-ssh root@bastion -o StrictHostKeyChecking=no "sudo find /root/ -iname ".openshift_install.log" -exec tail -1 {} \; " | awk '/password: /{ print $11 }'  | sed 's-"--g' | pbcopy
+ssh bastion "find /root/ -iname '.openshift_install.log' -exec tail -8 {} \; | head -n 1" | awk '{ print $12 }' | sed 's-\\"--g' | sed 's-"--g' | pbcopy
 open $(oc get route/console --namespace openshift-console --output jsonpath='{.spec.port.targetPort}://{.spec.host}')
