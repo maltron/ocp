0cef56b808863ae0a8ec9ad3a9917a34f8548264
Mauricio "Maltron" Leal <maltron@gmail.com>
2020-08-05 17:15:08 -0300

    Added 7th line of log

--- ocp-console.sh
+++ ocp-console.sh
@@ -1,2 +1,2 @@
-ssh bastion "find /root/ -iname '.openshift_install.log' -exec tail -8 {} \; | head -n 1" | awk '{ print $12 }' | sed 's-\\"--g' | sed 's-"--g' | pbcopy
+ssh bastion "find /root/ -iname '.openshift_install.log' -exec tail -7 {} \; | head -n 1" | awk '{ print $12 }' | sed 's-\\"--g' | sed 's-"--g' | pbcopy
 open $(oc get route/console --namespace openshift-console --output jsonpath='{.spec.port.targetPort}://{.spec.host}')
